8ff85e724dbcc6bc8b9cc1801d248b2569e2fc42
KlintonICT <chhunklinton91@gmail.com>
2024-06-26 10:45:07 +0700

    feat(client): create sign in form

--- client/src/pages/sign-in/index.tsx
+++ client/src/pages/sign-in/index.tsx
@@ -1,3 +1,52 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as yup from 'yup';
+
+interface FormValues {
+  username: string;
+  name: string;
+}
+
 export const SignIn = () => {
-  return <div>SignIn</div>;
+  const schema = yup.object().shape({
+    username: yup
+      .string()
+      .required('Username is Required')
+      .matches(/^[a-zA-Z0-9_.@$]+$/, 'Invalid Username'),
+    name: yup.string().required('Name is Required'),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver: yupResolver(schema) });
+
+  const onSubmit: SubmitHandler<FormValues> = (data, event) => {
+    event?.preventDefault();
+
+    const { username, name } = data;
+
+    console.log({ username, name });
+  };
+
+  return (
+    <div className='home'>
+      <h1>Welcome to Clubhouse Audio</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor='username'>Username: </label>
+          <input type='text' {...register('username')} />
+          {errors.username && (
+            <p style={{ color: 'red' }}>{errors.username.message}</p>
+          )}
+        </div>
+        <div>
+          <label htmlFor='name'>Name: </label>
+          <input type='text' {...register('name')} />
+          {errors.name && <p style={{ color: 'red' }}>{errors.name.message}</p>}
+        </div>
+        <button type='submit'>Sign In</button>
+      </form>
+    </div>
+  );
 };
