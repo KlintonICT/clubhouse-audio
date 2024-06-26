6ee67f87d1daca9dacf50985889aff5757cd810a
KlintonICT <chhunklinton91@gmail.com>
2024-06-26 11:36:11 +0700

    feat(server): setup server

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.post('/create-user', async (req: Request, res: Response) => {
+  const { username, name, image } = req.body;
+
+  if (!username || !name || !image) {
+    return res.status(400).json({ message: 'Required fields were empty' });
+  }
+});
+
+export default router;
