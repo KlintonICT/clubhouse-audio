34ac01a85d50ffe55ddf2f9a8b576ca0151e90aa
KlintonICT <chhunklinton91@gmail.com>
2024-06-27 16:02:43 +0700

    feat(client): create display partipants and leave the room if the room stop live

--- client/src/pages/room/controls.tsx
+++ client/src/pages/room/controls.tsx
@@ -1,11 +1,19 @@
+import { useCallStateHooks } from '@stream-io/video-react-sdk';
+
 import { LiveButton } from './live-button';
 import { MicButton } from './mic-button';
 
+import { useUserContext } from '@/contexts/user';
+
 export const Controls = () => {
+  const { useCallCreatedBy } = useCallStateHooks();
+  const createdBy = useCallCreatedBy();
+  const { user } = useUserContext();
+
   return (
     <div className='controls-panel'>
       <MicButton />
-      <LiveButton />
+      {user?.username === createdBy?.id && <LiveButton />}
     </div>
   );
 };
--- client/src/pages/room/index.tsx
+++ client/src/pages/room/index.tsx
@@ -3,15 +3,22 @@ import {
   useCallStateHooks,
   useRequestPermission,
 } from '@stream-io/video-react-sdk';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Controls } from './controls';
+import { Participants } from './participants';
 import { PermissionRequestPanel } from './permission-request';
 
 import { useUserContext } from '@/contexts/user';
 
 export const Room = () => {
-  const { useCallCustomData, useParticipants, useCallCreatedBy } =
-    useCallStateHooks();
+  const {
+    useCallCustomData,
+    useParticipants,
+    useCallCreatedBy,
+    useIsCallLive,
+  } = useCallStateHooks();
   const custom = useCallCustomData();
   const participants = useParticipants();
   const createdBy = useCallCreatedBy();
@@ -19,12 +26,23 @@ export const Room = () => {
     OwnCapability.SEND_AUDIO
   );
   const { user } = useUserContext();
+  const isLive = useIsCallLive();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLive && user?.username !== createdBy?.id) {
+      navigate('/');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLive]);
 
   return (
     <div className='room'>
       <h2 className='title'>{custom?.title ?? 'TITLE'}</h2>
       <h3 className='description'>{custom?.description ?? 'DESCRIPTION'}</h3>
       <p className='participant-count'>{participants.length} participants</p>
+
+      <Participants />
 
       {user?.username === createdBy?.id && <PermissionRequestPanel />}
 
